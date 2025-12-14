3f8b78209df6768fc5a89172b8557437d587fa5a
Erjan Gavalji <erjan.gavalji@gmail.com>
2025-12-14 12:35:18 +0200

    Do a recursive walk through the directory

--- main.go
+++ main.go
@@ -3,12 +3,25 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io/fs"
+	"path/filepath"
 )
 
 func main() {
 	var rootDir string
 	flag.StringVar(&rootDir, "root-dir", ".", "Root directory. Defaults to .")
 	flag.Parse()
+	err := filepath.Walk(rootDir, func(path string, info fs.FileInfo, err error) error {
+		if err != nil {
+			fmt.Printf("prevent panic by handling failure accessing %q: %v\n", path, err)
+			return err
+		}
+		fmt.Printf("visited file or dir: %q\n", path)
+		return nil
+	})
+	if err != nil {
+		fmt.Printf("Error walking the path %q: %v\n", rootDir, err)
+	}
 	fmt.Println(rootDir)
 
 }
