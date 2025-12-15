e08761050824cd6c9d9d9f104c157f60b4807996
Erjan Gavalji <erjan.gavalji@gmail.com>
2025-12-15 12:10:46 +0200

    Extract the image discovery to a separate function

--- main.go
+++ main.go
@@ -15,11 +15,9 @@ func isImage(path string) bool {
 	return slices.Contains(validExts, ext)
 }
 
-func main() {
-	var rootDir string
-	flag.StringVar(&rootDir, "root-dir", ".", "Root directory. Defaults to .")
-	flag.Parse()
-	err := filepath.Walk(rootDir, func(path string, info fs.FileInfo, err error) error {
+func readImages(rootDir string) (images []string, err error) {
+	var allImages []string
+	walkErr := filepath.Walk(rootDir, func(path string, info fs.FileInfo, err error) error {
 		if err != nil {
 			fmt.Printf("prevent panic by handling failure accessing %q: %v\n", path, err)
 			return err
@@ -29,14 +27,33 @@ func main() {
 			return nil
 		}
 		if isImage(path) {
-			fmt.Printf("image found: %s\n", path)
+			allImages = append(allImages, path)
 		}
 
 		return nil
 	})
+	if walkErr != nil {
+		return nil, walkErr
+	}
+	return allImages, nil
+}
+
+func main() {
+	var rootDir string
+	flag.StringVar(&rootDir, "root-dir", ".", "Root directory. Defaults to .")
+	flag.Parse()
+
+	allImages, err := readImages(rootDir)
+
 	if err != nil {
 		fmt.Printf("Error walking the path %q: %v\n", rootDir, err)
+		return
 	}
+
+	for _, imagePath := range allImages {
+		fmt.Printf("Found imaage under %q\n", imagePath)
+	}
+
 	fmt.Println(rootDir)
 
 }
