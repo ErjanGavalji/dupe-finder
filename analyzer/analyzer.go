8abbc2f828bb7194dd36d4a5d5cd88d297e7b708
Erjan Gavalji <erjan.gavalji@gmail.com>
2025-12-24 09:02:43 +0200

    Start building the dir map

--- analyzer/analyzer.go
+++ analyzer/analyzer.go
@@ -5,7 +5,17 @@ import (
 	"fmt"
 )
 
-func Drill(infos []imagereader.ImageInfo) string {
+// TODO: This might be totally unnecessary, as we will have the path
+//  duplicated, as it is used as a key of the map
+
+type DirInfo struct {
+	Path       string
+	ImageInfos []imagereader.ImageInfo
+}
+
+func Drill(infos []imagereader.ImageInfo) map[string]DirInfo {
+	dirInfos := make(map[string]DirInfo, 0)
+
 	fmt.Printf("Heeeey, this is someting from the analyzer package")
-	return ""
+	return dirInfos
 }
